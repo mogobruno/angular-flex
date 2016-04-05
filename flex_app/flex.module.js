a206afc3c5f195671e0f361f1a49665d2db8b02e
Bruno Silva <mogo.bruno@gmail.com>
2016-04-04 22:20:33 -0300

    added flex-item grow and order

--- flex_app/flex.module.js
+++ flex_app/flex.module.js
@@ -12,10 +12,14 @@
 
     angularFlex.constant('PROPERTIES', {
         'DISPLAY': 'display',
-        'DIRECTION':'flex-direction',
+        'DIRECTION': 'flex-direction',
         'WRAP': 'flex-wrap',
-        'JUSTIFY':'justify-content',
-        'ALIGN':'align-items',
+        'JUSTIFY': 'justify-content',
+        'ALIGN': 'align-items',
+        'CONTENT': 'align-content',
+        'GROW': 'flex',
+        'ORDER': 'order',
+        'PADDING':'padding'
     });
 
     angularFlex.constant('FLEXBOX', {
@@ -38,7 +42,7 @@
             'BETWEEN':'space-between',
             'AROUND':'space-around',
             'BASELINE':'baseline',
-            'STRECH':'stretch',
+            'STRETCH':'stretch',
         }
     })
 
@@ -47,28 +51,27 @@
             restrict: 'A',
             link: function (scope, element, attrs) {
                 
+                if(FLEXBOX.DIRECTION[direction])
+                    throw new Error('flexContainer needs a direction (e.g. flex-container="row")');
+                
                 var justify;
                 var align;
 
-                var wrap = (attrs.flexWrap)? attrs.flexWrap.toUpperCase() : undefined;
-
                 var direction = (attrs.flexContainer)?attrs.flexContainer.toUpperCase(): undefined;
-                
+                var wrap = (attrs.flexWrap)? attrs.flexWrap.toUpperCase() : undefined;
+                var content = (attrs.flexContent)? attrs.flexContent.toUpperCase(): undefined;
                 if(attrs.flexAlign) {
                     var aligns = attrs.flexAlign.toUpperCase().split(' ');
-                    justify = aligns[0].toUpperCase();
-                    align = aligns[1].toUpperCase();
+                    justify = (aligns[0])? aligns[0].toUpperCase() : undefined;
+                    align = (aligns[1])? aligns[1].toUpperCase() : undefined;
                 }
 
-                if(FLEXBOX.DIRECTION[direction]){
-                    element.css(PROPERTIES.DISPLAY, FLEXBOX.CONTAINER);
-                    element.css(PROPERTIES.DIRECTION, FLEXBOX.DIRECTION[direction]);
-                    element.css(PROPERTIES.WRAP, FLEXBOX.WRAP[wrap] || FLEXBOX.WRAP.WRAP);
-                    element.css(PROPERTIES.JUSTIFY, FLEXBOX.ALIGN[justify] || FLEXBOX.ALIGN.START);
-                    element.css(PROPERTIES.ALIGN, FLEXBOX.ALIGN[align] || FLEXBOX.ALIGN.START);
-                }else{
-                    throw new Error('flexContainer needs a direction (e.g. flex-container="row")');
-                }
+                element.css(PROPERTIES.DISPLAY, FLEXBOX.CONTAINER);
+                element.css(PROPERTIES.DIRECTION, FLEXBOX.DIRECTION[direction]);
+                element.css(PROPERTIES.WRAP, FLEXBOX.WRAP[wrap] || FLEXBOX.WRAP.WRAP);
+                element.css(PROPERTIES.JUSTIFY, FLEXBOX.ALIGN[justify] || FLEXBOX.ALIGN.START);
+                element.css(PROPERTIES.ALIGN, FLEXBOX.ALIGN[align] || FLEXBOX.ALIGN.START);
+                element.css(PROPERTIES.CONTENT, FLEXBOX.ALIGN[content] || FLEXBOX.ALIGN.START);
             }
         }
     });
@@ -77,7 +80,26 @@
         return {
             restrict: 'A',
             link: function (scope, element, attrs) {
-                element.css('color', 'red');
+                var grow = attrs.flexItem || 1;
+                var order = attrs.flexOrder || 1;
+                var shrink = 0;
+
+                if(grow > 100 || grow < 0)
+                    throw new Error('Invalid value to flex item');
+
+                element.css(PROPERTIES.GROW, grow+' '+shrink);
+                element.css(PROPERTIES.ORDER, order);
+                                
+            }
+        }
+    });
+
+    angularFlex.directive('fxPadding', function(PROPERTIES){
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                var padding = attrs.fxPadding || 1;
+                element.css(PROPERTIES.PADDING, padding+'em');                
             }
         }
     });
