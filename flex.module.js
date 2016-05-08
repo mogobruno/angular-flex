cb15a5d036eac32aaaa550756d4445b77b336483
Bruno Silva <mogo.bruno@gmail.com>
2016-05-07 23:25:20 -0300

    fix

--- flex.module.js
+++ flex.module.js
@@ -10,6 +10,29 @@
         'OPERA': '-o-'
     });
 
+    angularFlex.constant('WINDOW_SIZE', {
+        'XL': {
+            'MIN': 1920,
+            'MAX': 'INFINITY'
+        },
+        'LG': {
+            'MIN': 1280,
+            'MAX': 1919
+        },
+        'MD': {
+            'MIN': 960,
+            'MAX': 1279
+        },
+        'SM': {
+            'MIN': 600,
+            'MAX': 959
+        },
+        'XS': {
+            'MAX': 599,
+            'MIN': 'INFINITY'
+        }
+    });
+
     angularFlex.constant('PROPERTIES', {
         'DISPLAY': 'display',
         'DIRECTION': 'flex-direction',
@@ -46,63 +69,92 @@
         }
     })
 
-    angularFlex.directive('flexContainer', function(BROWSERS_PREFIX, PROPERTIES, FLEXBOX){
+    angularFlex.directive('flexContainer', function(){
         return {
             restrict: 'A',
-            link: function (scope, element, attrs) {
-                
-                if(FLEXBOX.DIRECTION[direction])
-                    throw new Error('flexContainer needs a direction (e.g. flex-container="row")');
-                
-                var justify;
-                var align;
-
-                var direction = (attrs.flexContainer)?attrs.flexContainer.toUpperCase(): undefined;
-                var wrap = (attrs.flexWrap)? attrs.flexWrap.toUpperCase() : undefined;
-                var content = (attrs.flexContent)? attrs.flexContent.toUpperCase(): undefined;
-                if(attrs.flexAlign) {
-                    var aligns = attrs.flexAlign.toUpperCase().split(' ');
-                    justify = (aligns[0])? aligns[0].toUpperCase() : undefined;
-                    align = (aligns[1])? aligns[1].toUpperCase() : undefined;
-                }
-
-                element.css(PROPERTIES.DISPLAY, FLEXBOX.CONTAINER);
-                element.css(PROPERTIES.DIRECTION, FLEXBOX.DIRECTION[direction]);
-                element.css(PROPERTIES.WRAP, FLEXBOX.WRAP[wrap] || FLEXBOX.WRAP.WRAP);
-                element.css(PROPERTIES.JUSTIFY, FLEXBOX.ALIGN[justify] || FLEXBOX.ALIGN.START);
-                element.css(PROPERTIES.ALIGN, FLEXBOX.ALIGN[align] || FLEXBOX.ALIGN.START);
-                element.css(PROPERTIES.CONTENT, FLEXBOX.ALIGN[content] || FLEXBOX.ALIGN.START);
-            }
+            controller: flexContainerController
         }
     });
 
-    angularFlex.directive('flexItem', function(BROWSERS_PREFIX, PROPERTIES, FLEXBOX){
+    angularFlex.directive('flexItem', function(){
         return {
             restrict: 'A',
-            link: function (scope, element, attrs) {
-                var grow = attrs.flexItem || 1;
-                var order = attrs.flexOrder || 1;
-                var shrink = 0;
-
-                if(grow > 100 || grow < 0)
-                    throw new Error('Invalid value to flex item');
-
-                element.css(PROPERTIES.GROW, grow+' '+shrink);
-                element.css(PROPERTIES.ORDER, order);
-                                
-            }
+            require: '^^flexContainer',
+            controller: flexItemController
         }
     });
 
     angularFlex.directive('fxPadding', function(PROPERTIES){
         return {
             restrict: 'A',
-            link: function (scope, element, attrs) {
-                var padding = attrs.fxPadding || 1;
-                element.css(PROPERTIES.PADDING, padding+'em');                
-            }
+            controller: fxPaddingController
         }
     });
+
+    angularFlex.service('WindowSize', function($window, WINDOW_SIZE){
+        this.change = function(resolve){
+            angular.element($window).bind('resize', function WindowResize(event){
+                Object.keys(WINDOW_SIZE).forEach(function(key){
+                    var SIZES = WINDOW_SIZE[key];
+                    if((SIZES.MIN === 'INFINITY' || SIZES.MIN <= window.innerWidth) &&
+                       (SIZES.MAX === 'INFINITY' || SIZES.MAX >= window.innerWidth)){
+                        resolve(key);
+                    }
+                });
+            });
+        };
+    });
+
+    function flexContainerController ($element, $attrs, BROWSERS_PREFIX, PROPERTIES, FLEXBOX) {
+        if(FLEXBOX.DIRECTION[direction])
+            throw new Error('flexContainer needs a direction (e.g. flex-container="row")');
+        
+        var justify;
+        var align;
+
+        var direction = upperCase($attrs.flexContainer);
+        var wrap = upperCase($attrs.flexWrap);
+        var content = upperCase($attrs.flexContent);
+        if($attrs.flexAlign) {
+            var aligns = $attrs.flexAlign.toUpperCase().split(' ');
+            justify = upperCase(aligns[0]);
+            align = upperCase(aligns[1]);
+        }
+
+        $element.css(PROPERTIES.DISPLAY, FLEXBOX.CONTAINER);
+        $element.css(PROPERTIES.DIRECTION, FLEXBOX.DIRECTION[direction]);
+        $element.css(PROPERTIES.WRAP, FLEXBOX.WRAP[wrap] || FLEXBOX.WRAP.WRAP);
+        $element.css(PROPERTIES.JUSTIFY, FLEXBOX.ALIGN[justify] || FLEXBOX.ALIGN.START);
+        $element.css(PROPERTIES.ALIGN, FLEXBOX.ALIGN[align] || FLEXBOX.ALIGN.START);
+        $element.css(PROPERTIES.CONTENT, FLEXBOX.ALIGN[content] || FLEXBOX.ALIGN.START);
+    }
+
+    function flexItemController($element, $attrs, BROWSERS_PREFIX, PROPERTIES, FLEXBOX, WindowSize) {
+        var grow = $attrs.flexItem || 1;
+        var order = $attrs.flexOrder || 1;
+        var shrink = 0;
+
+        WindowSize.change(function(window){
+            console.log("window "+window);
+        });
+
+        if(grow > 100 || grow < 0)
+            throw new Error('Invalid value to flex item');
+
+        $element.css(PROPERTIES.GROW, grow+' '+shrink);
+        $element.css(PROPERTIES.ORDER, order);               
+    }
+
+    function fxPaddingController($element, $attrs, PROPERTIES) {
+        var padding = $attrs.fxPadding || 1;
+        $element.css(PROPERTIES.PADDING, padding+'em');                   
+    }
+
+    function upperCase(value) {
+        return (value)? value.toUpperCase() : undefined;
+    }
+
+    
 
 
 }(angular))
